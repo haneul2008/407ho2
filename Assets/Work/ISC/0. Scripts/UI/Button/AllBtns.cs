a53365959860cf5b79d3e2d1c916ea86420c4ef0
Candy <dltkr0424@gmail.com>
2025-02-27 22:16:36 +0900

    UI

---
+++ Assets/Work/ISC/0. Scripts/UI/Button/AllBtns.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Work.ISC._0._Scripts.UI.Button
+{
+    public class AllBtns : MonoBehaviour
+    {
+        [SerializeField] private Image subPanel;
+
+        public void Play()
+        {
+            Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(0, 0, 0));
+            float yPos = dir.y;
+            subPanel.transform.DOMoveY(yPos, 0.5f);
+        }
+
+        public void Exit()
+        {
+            Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(0, -17, 0));
+            float yPos = dir.y;
+            subPanel.transform.DOMoveY(yPos, 0.5f);
+        }
+    }
+}
