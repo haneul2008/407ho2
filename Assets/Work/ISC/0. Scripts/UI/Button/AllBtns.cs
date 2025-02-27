2617f85b9e76f9b08754376b9f7233260fc918fe
Candy <dltkr0424@gmail.com>
2025-02-28 00:43:12 +0900

    라운디드패널

--- Assets/Work/ISC/0. Scripts/UI/Button/AllBtns.cs
+++ Assets/Work/ISC/0. Scripts/UI/Button/AllBtns.cs
@@ -7,7 +7,9 @@ namespace Work.ISC._0._Scripts.UI.Button
     public class AllBtns : MonoBehaviour
     {
         [SerializeField] private Image subPanel;
-
+        [SerializeField] private Image optionPanel;
+        
+        
         public void Play()
         {
             Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(0, 0, 0));
@@ -20,6 +22,20 @@ namespace Work.ISC._0._Scripts.UI.Button
             Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(0, -17, 0));
             float yPos = dir.y;
             subPanel.transform.DOMoveY(yPos, 0.5f);
+        }
+
+        public void Option()
+        {
+            Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(0, 0, 0));
+            float xPos = dir.x;
+            optionPanel.transform.DOMoveX(xPos, 0.5f);
+        }
+
+        public void OptionExit()
+        {
+            Vector3 dir = Camera.main.WorldToScreenPoint(new Vector3(30, 0, 0));
+            float xPos = dir.x;
+            optionPanel.transform.DOMoveX(xPos, 0.5f);
         }
     }
 }
