e63bfa3e1e16d382ec5c1187cdb7aa463fc38705
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2025-02-28 01:06:38 +0900

    add setting panel

--- Assets/Work/HN/Code/Save/SaveManager.cs
+++ Assets/Work/HN/Code/Save/SaveManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using Work.HN.Code.EventSystems;
@@ -38,6 +39,14 @@ namespace Work.HN.Code.Save
         public List<ObjectData> objectList = new List<ObjectData>();
         public string mapName = "Test";
     }
+
+    public class UserBuiltInData
+    {
+        public List<MapData> userMapList = new List<MapData>();
+        public float masterVolume;
+        public float bgmVolume;
+        public float sfxVolume;
+    }
     
     public class SaveManager : MonoBehaviour
     {
@@ -45,9 +54,23 @@ namespace Work.HN.Code.Save
         [SerializeField] private SaveData saveData;
         
         private MapData _mapData;
+        private UserBuiltInData _userData;
+        private string _path;
 
         private void Awake()
         {
+            _path = $"{Application.persistentDataPath}/GameData.json";
+
+            if (File.Exists(_path))
+            {
+                string json = File.ReadAllText(_path);
+                _userData = JsonUtility.FromJson<UserBuiltInData>(json);
+            }
+            else
+            {
+                _userData = new UserBuiltInData();
+            }
+            
             mapMakerChannel.AddListener<ObjectSaveEvent>(HandleObjectSave);
         }
 
@@ -72,6 +95,24 @@ namespace Work.HN.Code.Save
 
         private void FinishData()
         {
+            MapData mapData = GetUserMap(_mapData.mapName);
+            
+            if(mapData != null) _userData.userMapList.Remove(mapData);
+            
+            _userData.userMapList.Add(_mapData);
+        }
+
+        private MapData GetUserMap(string mapName)
+        {
+            foreach (MapData mapData in _userData.userMapList)
+            {
+                if (mapName.Trim() == mapData.mapName.Trim())
+                {
+                    return mapData;
+                }
+            }
+            
+            return null;
         }
 
         private void InitializeData()
@@ -83,6 +124,11 @@ namespace Work.HN.Code.Save
         {   
             string json = JsonUtility.ToJson(_mapData);
             saveData.DataSave(json);
+        }
+
+        public bool CanSaveData(string mapName)
+        {
+            return GetUserMap(mapName) == null;
         }
 
         private void SaveObject(EditorObject targetObject)
@@ -135,9 +181,9 @@ namespace Work.HN.Code.Save
             }
         }
 
-        public void GetMapData(int mapId)
+        public string GetMapName()
         {
-            
+            return _mapData.mapName;
         }
 
         [ContextMenu("TestLoad")]
