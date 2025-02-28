2aee0f8593aae1735f3769659028639805e83f3f
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2025-02-28 15:21:46 +0900

    add gameClearUi

--- Assets/Work/HN/Code/MapMaker/UI/GameClearUI.cs
+++ Assets/Work/HN/Code/MapMaker/UI/GameClearUI.cs
@@ -1,11 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Work.HN.Code.Save;
 
 namespace Work.HN.Code.MapMaker.UI
 {
     public class GameClearUI : MonoBehaviour
     {
-        [SerializeField] private string mapName;
+        [SerializeField] private TextMeshProUGUI mapNameText;
+        [SerializeField] private float yPosInActive = 0, duration = 0.5f;
+
+        private float yPosInDeactivate;
+        private RectTransform _rectTrm;
+
+        private void Awake()
+        {
+            _rectTrm = transform as RectTransform;
+            yPosInDeactivate = _rectTrm.anchoredPosition.y;
+        }
+
+        public void Active(bool isActive)
+        {
+            if (isActive)
+            {
+                _rectTrm.DOAnchorPosY(yPosInActive, duration);
+            }
+            else
+            {
+                _rectTrm.DOAnchorPosY(yPosInDeactivate, duration);
+            }
+        }
         
-        
+        public void HandleMapLoaded(MapData mapData)
+        {
+            mapNameText.text = mapData.mapName;
+        }
     }
 }
