4ac24aed52e870062dcca97afa2ed1f70d9c891b
pormer <juwonst@gmail.com>
2025-02-28 15:41:41 +0900

    outline shader 제작 & 연동

--- Assets/Work/JW/Code/TriggerSystem/Trigger.cs
+++ Assets/Work/JW/Code/TriggerSystem/Trigger.cs
@@ -9,16 +9,19 @@ namespace Work.JW.Code.TriggerSystem
     [RequireComponent(typeof(BoxCollider2D))]
     public abstract class Trigger : MonoBehaviour, ITriggerEvent
     {
+        private static readonly int ColorDataHash = Shader.PropertyToID("_ColorData");
         public int TargetID { get; set; } //누굴 조종할지의 ID
         public int? TriggerID { get; set; } //자신의 트리거 ID
         [SerializeField] private Vector2 triggerSize;
         private BoxCollider2D _collider;
+        private SpriteRenderer _spriter;
         protected Transform[] _targets;
         protected bool _isTrigger;
         
         protected virtual void Awake()
         {
             _collider = GetComponent<BoxCollider2D>();
+            _spriter = GetComponentInChildren<SpriteRenderer>();
             _collider.isTrigger = true;
         }
 
@@ -36,6 +39,11 @@ namespace Work.JW.Code.TriggerSystem
         {
             triggerSize = size;
             _collider.size = triggerSize;
+        }
+
+        public void SetOutLineColor(Color color)
+        {
+            _spriter.material.SetColor(ColorDataHash, color);
         }
 
 
