d104cd787331c52098570f3d9dae393b83ff1bef
haneul2008 <157570674+haneul2008@users.noreply.github.com>
2025-02-28 15:10:06 +0900

    add inGame loader

---
+++ Assets/Work/HN/Code/MapMaker/UI/GameClearUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Work.HN.Code.MapMaker.UI
+{
+    public class GameClearUI : MonoBehaviour
+    {
+        [SerializeField] private string mapName;
+        
+        
+    }
+}
---
+++ Assets/Work/HN/Code/Save/InGameLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Work.ISC._0._Scripts.Save.ExelData;
+
+namespace Work.HN.Code.Save
+{
+    public class InGameLoader : MonoBehaviour
+    {
+        public UnityEvent<MapData> OnMapLoaded;
+        
+        [SerializeField] private SaveData saveData;
+        
+        private void Awake()
+        {
+            int sequence = DataReceiver.Instance.UserMapDataSequence;
+            GetMapData(sequence);
+        }
+
+        private void GetMapData(int seq)
+        {
+            saveData.DataLoad($"B{seq}", data =>
+            {
+                string json = data;
+                MapData mapData = JsonUtility.FromJson<MapData>(json);
+                
+                OnMapLoaded?.Invoke(mapData);
+            });
+        }
+    }
+}
