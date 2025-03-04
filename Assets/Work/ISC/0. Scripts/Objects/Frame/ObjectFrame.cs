7fddb1b9919292cdcbe88187ea3bfcc5c386e138
haneul2008 <littlehn08@gmail.com>
2025-03-04 18:30:36 +0900

    add chainsaw animation, fix object frame collider

--- Assets/Work/ISC/0. Scripts/Objects/Frame/ObjectFrame.cs
+++ Assets/Work/ISC/0. Scripts/Objects/Frame/ObjectFrame.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Work.JW.Code.Entities;
-using Work.JW.Code.Entities.Player;
 using Work.JW.Code.TriggerSystem;
 
 namespace Work.ISC._0._Scripts.Objects.Frame
 {
     public class ObjectFrame : MonoBehaviour, ITriggerEvent
-    { 
+    {
         private int _id;
 
         public int ID
@@ -20,45 +18,49 @@ namespace Work.ISC._0._Scripts.Objects.Frame
                 _id = value;
             }
         }
-        
+
         private void IDSetting(int id)
         {
             _objSO = ObjectSOList[id - 1];
-            
+
             InitializeObject();
         }
-        
-        [field : SerializeField] public List<ObjectFrameSO> ObjectSOList { get; private set; }
+
+        [field: SerializeField] public List<ObjectFrameSO> ObjectSOList { get; private set; }
 
         [SerializeField] private ObjectFrameSO _objSO;
-        
-        
+
+
         public SpriteRenderer SpriteCompo { get; protected set; }
-        public BoxCollider2D ColliderCompo { get; protected set; }
+        public Collider2D ColliderCompo { get; protected set; }
 
         private void Awake()
         {
             SpriteCompo = GetComponent<SpriteRenderer>();
-            ColliderCompo = GetComponent<BoxCollider2D>();
-            
+
             InitializeObject();
         }
 
         private void InitializeObject()
         {
             if (_objSO == null) return;
-            
+
             _objSO.InitializeObject();
 
             AfterInitObj();
         }
-        
+
         private void Start()
         {
             if (_objSO.GetType() == typeof(StartPointObjectSO))
             {
                 StartPointObjectSO startPointSO = (StartPointObjectSO)_objSO;
                 startPointSO.TargetToPosition(transform);
+            }
+            else if (_objSO is ChainsawFramSO chainsawData)
+            {
+                Animator animator = gameObject.AddComponent<Animator>();
+                animator.runtimeAnimatorController = chainsawData.animationController;
             }
         }
 
@@ -69,12 +71,27 @@ namespace Work.ISC._0._Scripts.Objects.Frame
 
         private void AfterInitObj()
         {
+            ColliderCompo = _objSO.colliderType switch
+            {
+                ColliderEnum.Box => gameObject.AddComponent<BoxCollider2D>(),
+                ColliderEnum.Circle => gameObject.AddComponent<CircleCollider2D>(),
+                _ => null
+            };
+
             gameObject.name = _objSO.name;
             gameObject.layer = _objSO.ObjectLayer;
             SpriteCompo.sprite = _objSO.ObjectImage;
             ColliderCompo.offset = _objSO.ColliderOffset;
-            ColliderCompo.size = _objSO.ColliderSize;
             ColliderCompo.isTrigger = _objSO.isTrigger;
+
+            if (ColliderCompo is BoxCollider2D boxCollider)
+            {
+                boxCollider.size = _objSO.ColliderSize;
+            }
+            else if (ColliderCompo is CircleCollider2D circleCollider)
+            {
+                circleCollider.radius = _objSO.CircleColliderRadius;
+            }
         }
     }
 }
