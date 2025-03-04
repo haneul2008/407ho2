1e96ac526bebae0bc23e0a43d93b1d133c1dc0ee
haneul2008 <littlehn08@gmail.com>
2025-03-04 12:40:52 +0900

    add delete btn

--- Assets/Work/HN/Code/Test/TestMapUI.cs
+++ Assets/Work/HN/Code/Test/TestMapUI.cs
@@ -3,6 +3,7 @@ using Ami.BroAudio;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Work.HN.Code.EventSystems;
 using Work.HN.Code.Save;
 
 namespace Work.HN.Code.Test
@@ -10,7 +11,8 @@ namespace Work.HN.Code.Test
     public class TestMapUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI mapNameText, registeredText;
-        [SerializeField] private GameObject editBtn;
+        [SerializeField] private GameEventChannelSO titleChannel;
+        [SerializeField] private GameObject editBtn, deleteBtn;
         [SerializeField] private SoundID clickSoundID;
 
         private string _mapName;
@@ -23,8 +25,22 @@ namespace Work.HN.Code.Test
             registeredText.text = mapData.isRegistered ? "맵이 등록됨" : "";
             
             editBtn.SetActive(!mapData.isRegistered);
+            deleteBtn.SetActive(!mapData.isRegistered);
+
+            titleChannel.AddListener<MapDeleteEvent>(HandleMapDelete);
         }
-        
+
+        private void OnDestroy()
+        {
+            titleChannel.RemoveListener<MapDeleteEvent>(HandleMapDelete);
+        }
+
+        private void HandleMapDelete(MapDeleteEvent evt)
+        {
+            if (_mapName == evt.mapData.mapName)
+                Destroy(gameObject);
+        }
+
         public void OnEditBtnClick()
         {
             BroAudio.Play(clickSoundID);
@@ -37,6 +53,13 @@ namespace Work.HN.Code.Test
         {
             DataReceiver.Instance.SetPlayMapData(_mapName);
             SceneManager.LoadScene("JW");
+        }
+
+        public void OnDeleteBtnClick()
+        {
+            DeleteRequestEvent requestEvt = TitleEvent.DeleteRequestEvent;
+            requestEvt.mapName = _mapName;
+            titleChannel.RaiseEvent(requestEvt);
         }
     }
 }
