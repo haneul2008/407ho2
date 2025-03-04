75e21e62a12987e0b1e3d4803dea47a20258d914
haneul2008 <littlehn08@gmail.com>
2025-03-04 22:41:06 +0900

    commit

---
+++ Assets/Work/JW/Code/MapLoad/UI/NetworkFailPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ami.BroAudio;
+using UnityEngine;
+
+namespace Work.JW.Code.MapLoad.UI
+{
+    public class NetworkFailPanel : MonoBehaviour
+    {
+        [SerializeField] private SoundID clickSoundID;
+        [SerializeField] private SoundID failSoundID;
+        
+        private void Awake()
+        {
+            Active(false);
+        }
+
+        public void FailNetwork()
+        {
+            BroAudio.Play(failSoundID);
+        }
+
+        public void ClickAudio()
+        {
+            BroAudio.Play(clickSoundID);
+        }
+
+        public void Active(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+    }
+}
