13de1ca24a7915b41ed24e09dd5fe86181fadda5
pormer <juwonst@gmail.com>
2025-03-10 23:31:08 +0900

    각자 카메라 연동

---
+++ Assets/Work/JW/Code/Network/NetworkFollowCameraSetting.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Cinemachine;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Code.Network
+{
+    public class NetworkFollowCameraSetting : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera followCam;
+        private Transform _target;
+        
+        public void SetFollowTarget()
+        {
+            _target = NetworkManager.Singleton.LocalClient.PlayerObject.transform;
+            followCam.Follow = _target;
+        }
+    }
+}
